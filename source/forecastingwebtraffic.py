93dc31c27f4bf7a1a49ce267148652f5f757cd6f
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-06 21:12:19 +0530

    update

--- source/forecastingwebtraffic.py
+++ source/forecastingwebtraffic.py
@@ -19,3 +19,131 @@ data=pd.read_csv('data/webtraffic.csv')
 
 print(data.head())
 
+#Check the shape of the data
+print(data.shape)
+
+## Data Exploration
+import matplotlib.pyplot as plt
+sessions = data['Sessions'].values
+
+ar = np.arange(len(sessions))
+plt.figure(figsize=(22,10))
+plt.plot(ar, sessions,'r')
+plt.show()
+
+#first week web traffic
+sample = sessions[:168]
+ar = np.arange(len(sample))
+plt.figure(figsize=(22,10))
+plt.plot(ar, sample,'r')
+plt.show()
+
+'''
+## Data Preparation
+
+We will model the problem to predict the traffic for the next hour based on the previous week data i.e. 168 hours. Lets define a function to prepare the input and output data accordingly.
+
+*Note: The input is a sequence of values and the output is a single value.*
+
+
+'''
+def prepare_data(seq,num):
+  x=[]
+  y=[]
+
+  for i in range(0,(len(seq)-num),1):
+    
+    input_ = seq[i:i+num]
+    output  = seq[i+num]
+    
+    x.append(input_)
+    y.append(output)
+    
+  return np.array(x), np.array(y)
+
+#Call the function
+num=168
+x,y= prepare_data(sessions,num)
+
+print(len(x))
+
+print(x[0])
+
+print(y[0])
+
+#Split the dataset into training and validation data
+ind = int(0.9 * len(x))
+
+x_tr = x[:ind]
+y_tr = y[:ind]
+
+x_val=x[ind:]
+y_val=y[ind:]
+
+#Normalize the input and output data as it speeds up the training process
+
+from sklearn.preprocessing import StandardScaler
+
+#normalize the inputs
+x_scaler= StandardScaler()
+x_tr = x_scaler.fit_transform(x_tr)
+x_val= x_scaler.transform(x_val)
+
+#reshaping the output for normalization
+y_tr=y_tr.reshape(len(y_tr),1)
+y_val=y_val.reshape(len(y_val),1)
+
+#normalize the output
+y_scaler=StandardScaler()
+y_tr = y_scaler.fit_transform(y_tr)[:,0]
+y_val = y_scaler.transform(y_val)[:,0]
+
+print(x_tr.shape)
+
+'''
+As you can see here, the input data is a 2 dimenional array but the LSTM and Conv1D accepts only 3 dimensional inputs in the form of (no. of samples, no. of timesteps, no. of features)
+
+So, reshaping the input data as per the model requirement
+
+'''
+#reshaping input data
+x_tr= x_tr.reshape(x_tr.shape[0],x_tr.shape[1],1)
+x_val= x_val.reshape(x_val.shape[0],x_val.shape[1],1)
+
+print(x_tr.shape)
+
+'''
+Now, the data is ready for model training. 
+
+<br>
+
+## Model Building
+
+Define the model architecture
+'''
+
+from keras.models import *
+from keras.layers import *
+from keras.callbacks import *
+
+model= Sequential()
+
+model.add(Conv1D(64, 3, padding='same', activation='relu',input_shape=(num,1)))
+
+model.add(Conv1D(32, 5, padding='same', activation='relu',input_shape=(num,1)))
+
+model.add(Flatten())
+
+model.add(Dense(64,activation='relu'))
+model.add(Dense(1,activation='linear'))
+
+model.summary()
+
+model.compile(loss='mse',optimizer='adam')
+
+
+mc = ModelCheckpoint('best_model.hdf5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+
+history=model.fit(x_tr, y_tr ,epochs=30, batch_size=32, validation_data=(x_val,y_val), callbacks=[mc])
+
